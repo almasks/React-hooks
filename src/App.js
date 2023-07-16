cfd5beaabfbe8c871feca39b38b8d39adcd6a6d3
Almas K S <mailmealmasks@gmail.com>
2023-07-16 21:04:40 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './App.css';
-import ComponentC from './components/ComponentC';
-export const UseContext=React.createContext()
-export const ChannelContext=React.createContext()
+import CounterOne from './components/CounterOne';
 
 
 function App() {
   return (
     <div className="App">
 
-      <UseContext.Provider value={'vishwas'}>
-        <ChannelContext.Provider value={'codeevolution'}>
-          <ComponentC/>
-          </ChannelContext.Provider>
-        </UseContext.Provider>
+      <CounterOne/>
     </div>
   );
 }
